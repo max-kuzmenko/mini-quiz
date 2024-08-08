82089121a296515d9b9a533591306071565e52bf
Max Kuzmenko <kuzmenko.maks.1999@gmail.com>
2024-08-08 10:51:46 +0300

    setup: add api, add launch json

---
+++ src/api/questions.ts
@@ -0,0 +1,22 @@
+
+
+export type TAnswer = {
+    text: string;
+    id: string;
+}
+
+export type TQuestion = {
+    id: string;
+    question: string;
+    answers: TAnswer[];
+}
+
+export const getQuestions = async (): Promise<TQuestion> => {
+    const response = await fetch('https://mocki.io/v1/8bc53604-afb0-4ba0-9303-69302a963b79');
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch questions');
+    }
+
+    return response.json();
+}
